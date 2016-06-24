4709fef1dbdec4eb40f511456ae49d7e0ae39b62
Deng Han <han.deng@ansys.com>
2016-06-24 15:11:28 +0800

    version on 24th June

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,11 +1,18 @@
+require 'date'
 class StaticPagesController < ApplicationController
   skip_before_filter  :verify_authenticity_token
   def home
         @user = VirtualUser.find_by(user_session: session.id)
-        if @user
-                @qa_suite_today = QaSuite.homepage_qa_suites_today(@user.version, @user.platform, @user.timestamp).all
+        if not @user.present?
+                redirect_to '/'
         else
-                @qa_suite_today = QaSuite.homepage_qa_suites_today('master', 'linux_x86_64_rhel6').all
+                if @user.build_branch.nil?
+                        @qa_suite_today = QaSuite.homepage_qa_suites_today(@user.version, @user.platform, @user.timestamp).all
+                        @build_step_today = BuildStep.homepage_build_steps_today(@user.version, @user.platform, @user.timestamp).all
+                else
+                        @qa_suite_today = QaSuite.homepage_qa_suites_by_branch(@user.version, @user.platform, @user.build_branch)
+                        @build_step_today = BuildStep.homepage_build_steps_by_branch(@user.version, @user.platform, @user.build_branch)
+                end
         end
         #respond_to do |format|
         #       format.html { redirect_to root_url }
@@ -22,6 +29,9 @@ class StaticPagesController < ApplicationController
                         user.version = params[:version]
                 elsif params.keys[0] == 'platform'
                         user.platform = params[:platform]
+                        user.build_branch = QaSuite.where("version = ?", user.version).where("platform = ?", params[:platform]).where("suite_name = ?", 'customer').first.branch
+                elsif params.keys[2] == 'commit' && params[:commit] == 'Go'
+                        user.build_branch = params[:select][:buildname]
                 elsif params.keys[1] == 'commit'
                         if params[:commit] == 'Prev'
                                 user.timestamp = (user.timestamp.to_datetime - 1.days).to_s
@@ -29,20 +39,21 @@ class StaticPagesController < ApplicationController
                                 user.timestamp = (user.timestamp.to_datetime + 1.days).to_s
                         end
                 end
+
                 if user.save!
+                        if user.build_branch.nil?
+                                @selected_qa_suite_today = QaSuite.homepage_qa_suites_today(user.version, user.platform, user.timestamp).all
+                                @selected_build_step_today = BuildStep.homepage_build_steps_today(user.version, user.platform, user.timestamp).all
+                        else
+                                @selected_qa_suite_today = QaSuite.homepage_qa_suites_by_branch(user.version, user.platform, user.build_branch)
+                                @selected_build_step_today = BuildStep.homepage_build_steps_by_branch(user.version, user.platform, user.build_branch)
+
+                        end
+                        @selected_user = VirtualUser.find_by(user_session: session.id)
                         respond_to do |format|
-                                format.html { redirect_to '/homepage' }
-                                format.js
+                                format.html { redirect_to '/' }
+                                format.js   {}
                         end
                 end
         end
   end
-
-  def rhel5
-        @qa_suite_today = QaSuite.homepage_qa_suites_today_rhel5.all
-        @qa_suite_yesterday = QaSuite.homepage_qa_suites_yesterday_rhel5.all
-        @build_step_today = BuildStep.homepage_build_steps_today_rhel5.all
-        @build_step_yesterday = BuildStep.homepage_build_steps_yesterday_rhel5.all
-  end 
-             
-end
--- app/models/qa_suite.rb
+++ app/models/qa_suite.rb
@@ -20,82 +20,115 @@ class QaSuite < ActiveRecord::Base
 	end
 
 	def self.get_suite_data(suite_logfile)
-		suite_info = Hash.new
-		pass_number = 0
-		failed_number = 0
-		tc_info_index = []
-		times_hash = Hash.new
-		suite_info[:log_file] = suite_logfile
-		suite_info[:version], suite_info[:platform], suite_info[:suite_name], suite_info[:branch], suite_info[:build_type] = QaSuite.pre_parse_logfile(suite_logfile)
-		File.open(suite_logfile, "r") do |file|
-			while line = file.gets
-				if not line =~ /^$/
-					if line =~ /.+\sTESTCASE\s.+/
-						c_info = Hash.new
-						c_info[:case_name] = line[/.projs00.+/].split('/')[-1].gsub(/.pyt/, '')
-						c_info[:group] = line[/.projs00.+/].split('/')[-2]
-						c_info[:suite_name], c_info[:branch] = suite_info[:suite_name], suite_info[:branch]
-						c_info[:s_time] = QaSuite.parse_time(line[/^.+[0-9]{2}:[0-9]{2}/])
-						c_info[:host] = line[/sjo[a-zA-Z]+[0-9]+\W[0-9]+/]					
-						c_info[:qa_log_path] = line[/.projs00.+/].gsub(/.pyt/, '.log') #need
-						c_info[:status] = 'Passed'
-						tc_info_index << c_info
-						pass_number += 1
-					elsif line =~ /^FAILED.+file.+/
-						c_info = Hash.new
-						c_info[:case_name] = line[/.projs00.+/].split('/')[-1].gsub(/.log/, '')
-						c_info[:group] = line[/.projs00.+/].split('/')[-2]
-						c_info[:suite_name], c_info[:branch] = suite_info[:suite_name], suite_info[:branch]
-#						c_info[:s_time] = QaSuite.parse_time(line[/^.+[0-9]{2}:[0-9]{2}/])
-#						c_info[:start_time] = parse_time(line[/^.+[0-9]{2}:[0-9]{2}/])
-#						c_info[:host] = line[/sjo[a-zA-Z]+[0-9]+\W[0-9]+/]
-						c_info[:status] = 'Failed'					
-						c_info[:qa_log_path] = line[/.projs00.+/]
-						tc_info_index << c_info
-						failed_number += 1
-					elsif line =~/^\s+[0-9]+\W[0-9]{2}.+/
-						times_hash[line.split('/')[-1].split('.')[0..-2].join('.')] = line.split('.')[0].strip
-					elsif line =~ /^START\sTIME+/
-						suite_info[:start_time]	= QaSuite.strp_time(line[/[0-9]{2}.+$/])
-						suite_info[:date] = Date.today.to_s
-					elsif line =~ /^PID.+/
-						suite_info[:pid] = line[/[0-9]+/]
-					elsif line =~ /^DISPLAY.+/
-						suite_info[:display] = line[/sjoth.+/]
-					elsif line =~ /^HOST.+/
-						suite_info[:host] = line[/sjo.+/]
-					elsif line =~ /^EXECUTABLE.+/
-						suite_info[:executable] = line[/\Wprojs00.+/]
-					elsif line =~ /^COMMAND\sLINE.+/
-						suite_info[:command_line] = line[/\W{2}runqa.+/]
-					end
-				end
-			end
-			suite_info[:pass_number] = pass_number - failed_number
-			suite_info[:failed_numnber] = failed_number
-		end
-		tc_info_index.each do |h|
-			if not h[:s_time].nil?
-				h[:e_time] = h[:s_time] + times_hash[h[:case_name]].to_i.seconds
-			end
-		end
-		return suite_info, tc_info_index
+                suite_info = Hash.new
+                pass_number = 0
+                failed_number = 0
+                tc_info_index = Array.new
+                times_hash = Hash.new
+                summary_begin_or_not = false
+                suite_logs = String.new
+                suite_summary = String.new
+                suite_info[:log_file] = suite_logfile
+                suite_info[:version], suite_info[:platform], suite_info[:suite_name], suite_info[:branch], suite_info[:build_type] = QaSuite.pre_parse_logfile(suite_logfile)
+                File.open(suite_logfile, "r") do |file|
+                        while line = file.gets
+                                if summary_begin_or_not
+                                        if line =~ /^FAILED.+file:.+/ or line =~ /^QA_WARNING\s+file:/ or line =~ /^DIFF\s+file:.+/
+                                                suite_logs << line[/.projs00.+/] + ','
+                                                suite_summary << line.gsub(/file:.+/, '<Pre><a href="http://sjorhqa128-1:8000/' + line[/file:.+/] + '.html">File</a><pre>')
+                                        else
+                                                suite_summary << line
+                                        end
+                                end
+                                if not line =~ /^$/
+                                        if line =~ /.+\sTESTCASE\s.+/
+                                                c_info = Hash.new
+                                                c_info[:case_name] = line[/.projs00.+/].split('/')[-1]
+                                                c_info[:group] = line[/.projs00.+/].split('/')[-2]
+                                                c_info[:suite_name], c_info[:branch] = suite_info[:suite_name], suite_info[:branch]
+                                                c_info[:s_time] = QaSuite.parse_time(line[/^.+[0-9]{2}:[0-9]{2}/])
+                                                c_info[:host] = line[/sjo[a-zA-Z]+[0-9]+\W[0-9]+/]
+                                                c_info[:qa_log_path] = line[/.projs00.+/].gsub(/.pyt/, '/run.log')
+                                                c_info[:path_exist_or_not] = File.exist?(c_info[:qa_log_path])
+                                                c_info[:status] = 'Passed'
+                                                tc_info_index << c_info
+                                                pass_number += 1
+                                        elsif line =~ /^FAILED\s+file.+/ or line =~ /^QA_WARNING\s+file:.+/ or line =~ /^DIFF\s+file:.+/
+                                                #c_info = Hash.new
+                                                #n_info = Hash.new
+                                                #if n_info = tc_info_index.find {|x| x[:case_name] == line[/.projs00.+/].split('/')[-1].gsub(/.log/, '')}
+                                                #       c_info[:s_time] = n_info[:s_time]
+                                                #end
+                                                if tc_info_index.find {|x| x[:case_name] == line[/.projs00.+/].split('/')[-1].split('.')[0] + '.pyt'}
+                                                        if tc_info_index.find {|x| x[:case_name] == line[/.projs00.+/].split('/')[-1].split('.')[0] + '.pyt'}[:status] == 'Passed'
+                                                                tc_info_index.find {|x| x[:case_name] == line[/.projs00.+/].split('/')[-1].split('.')[0] + '.pyt'}[:status]='Failed'
+                                                                failed_number += 1
+                                                        end
+                                                end
+                                        elsif line =~/^\s+[0-9]+\W[0-9]{2}.+/
+                                                times_hash[line.split('/')[-1].split('.')[0..-2].join('.') + '.pyt'] = line.split('.')[0].strip.to_i + 1
+                                        elsif line =~ /^START\sTIME+/
+                                                suite_info[:start_time] = QaSuite.strp_time(line[/[0-9]{2}.+$/])
+                                                suite_info[:date] = Date.today.to_s
+                                        elsif line =~ /^PID.+/
+                                                suite_info[:pid] = line[/[0-9]+/]
+                                        elsif line =~ /^DISPLAY.+/
+                                                suite_info[:display] = line[/sjoth.+/]
+                                        elsif line =~ /^HOST.+/
+                                                suite_info[:host] = line[/sjo.+/]
+                                        elsif line =~ /^EXECUTABLE.+/
+                                                suite_info[:executable] = line[/\Wprojs00.+/]
+                                        elsif line =~ /^COMMAND\sLINE.+/
+                                                suite_info[:command_line] = line[/\W{2}runqa.+/]
+                                        elsif line =~ /^SUMMARY.+/
+                                                if line =~ /.+UH\sOH.+/
+                                                        suite_info[:status] = 'Failed'
+                                                else
+                                                        suite_info[:status] = 'Passed'
+                                                end
+                                        elsif line =~ /^TOTAL\sRUN.+/
+                                                summary_begin_or_not = true
+                                                suite_summary << line
+                                        end
+                                end
+                        end
+                        suite_info[:pass_number] = pass_number - failed_number
+                        suite_info[:failed_numnber] = failed_number
+                end
+                suite_info[:summary] = suite_summary
+                suite_info[:failed_info] = suite_logs
+                tc_info_index.each do |h|
+                        if not h[:s_time].nil? and not times_hash[h[:case_name]].nil?
+                                h[:e_time] = h[:s_time] + times_hash[h[:case_name]].seconds
+                        end
+                end
+                return suite_info, tc_info_index
 	end
 
-	def self.parse_time(string)
-		return DateTime.parse(string)
-	end
+		def self.parse_time(string)
+                return DateTime.parse(string)
+        end
 
-	def self.strp_time(string)
-		return DateTime.strptime(string,'%m/%d/%y %H:%M:%S')
-		#DateTime.strptime('05/02/16 03:58:26','%m/%d/%y %H:%M:%S')
-	end
+        def self.strp_time(string)
+                return DateTime.strptime(string,'%m/%d/%y %H:%M:%S')
+                #DateTime.strptime('05/02/16 03:58:26','%m/%d/%y %H:%M:%S')
+        end
+
         def self.pre_parse_logfile(logfile)
-            return 'master', 'linux_x86_64_rhel6', 'test_suite', 'test_branch', 'test_type'
+                return String.new(logfile).split('/')[5].downcase, String.new(logfile).split('/')[4], String.new(logfile).split('/')[-2], String.new(logfile).split('/')[6],
+ String.new(logfile).split('/')[-5]
         end
 
         def self.homepage_qa_suites_today(version, platform, timestamp)
-                QaSuite.where("date = ?", timestamp.split('T')[0]).where("version = ?", version.downcase).where("platform = ?", platform)
+                if timestamp.nil?
+                        QaSuite.where("date = ?", Date.today.to_s).where("version = ?", version.downcase).where("platform = ?", platform)
+                else
+                        QaSuite.where("date = ?", timestamp.split('T')[0]).where("version = ?", version.downcase).where("platform = ?", platform)
+                end
+        end
+
+        def self.homepage_qa_suites_by_branch(version, platform, branch)
+                        QaSuite.where("version = ?", version.downcase).where("platform = ?", platform).where("branch = ?", branch)
         end
 
         def self.homepage_qa_suites_today_rhel5
